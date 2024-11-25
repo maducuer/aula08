0a4df4e8a445952a6cd4028153ebfba99db2a6dc
maducuer <maducuer@gmail.com>
2024-11-25 14:34:15 -0400

    terminar
    terminar

--- cliente/src/pages/Registro.jsx
+++ cliente/src/pages/Registro.jsx
@@ -1,9 +1,33 @@
 import { useState } from "react";
 
 export default function Registrar() {
+ const [nome,setNome] = useState('');
+ const [email,setEmail] = useState('');
 
+ const Registrar = (event) => {
+ event.preventDefault();
+ await fetch("http://localhost:3000/usuarios",{
+  method: 'POST',
+  headers: {'content-type': 'Application/json'},
+  body: JSON.stringify({nome:nome, email:email
+  })
+ })
+ }
 
   return (
-        
+        <main>
+          <form action="" onSubmit={Registrar}>
+
+            <input type="text" 
+            value={nome} 
+            onChange={(event) => setNome(event.target.value)}/>
+
+            <input type="email" 
+            value={email}
+             onChange={(event) => setEmail(event.target.value)}/>
+
+            <button>Salvar</button>
+          </form>
+        </main>
   );
 }
